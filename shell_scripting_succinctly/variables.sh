9ac0441ae220315e6bcc1b6facc61a98515f9e0f
silloi <silloi1993@gmail.com>
2022-07-25 19:42:31 +0900

    first commit

---
+++ shell_scripting_succinctly/variables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+MY_SHELL="bash"
+echo "I like the $MY_SHELL shell."
+echo "I like the ${MY_SHELL} shell."
+
+echo "I am ${MY_SHELL}ing on my keyboard."
+echo "I am $MY_SHELLing on my keyboard."
+
+SERVER_NAME=$(hostname)
+# SERVER_NAME=`hostname`
+echo "You are running this script on ${SERVER_NAME}."
+
+FIRST3LETTERS="ABC"
+FIRST_THREE_LETTERS="ABC"
+firstThreeLetters="ABC"
+
